5e547b25f1ef0233ec656f60e782c0d47f2e966b
viastolfi <vincent.astolfi@etu.uca.fr>
2023-01-10 16:42:31 +0100

    Day 2 Exo 1 2022

---
+++ 2022/2_1/exo.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+
+int compare(char opp, char my);
+
+int main(void){
+	vector<char>* vect = new vector<char>;
+	int total = 0;
+
+	for (std::string line; std::getline(std::cin, line);) {
+		for(char& c : line){
+			if(c != ' '){
+				vect->push_back(c);
+			}
+		}
+    }
+
+	for(std::vector<char>::iterator it = vect->begin(); it!=vect->end(); it=it+2){
+		total = total + compare(*it, *(it+1));
+	}
+
+	std::cout << total << std::endl;
+
+    return 0;	
+}
+
+int compare(char opp, char my){
+	switch(opp){
+		case 'A':
+			switch(my){
+				case 'X':
+					return 4;
+				case 'Y':
+					return 8;
+				case 'Z':
+					return 3;
+			}
+		case 'B':
+			switch(my){
+				case 'X':
+					return 1;
+				case 'Y':
+					return 5;
+				case 'Z':
+					return 9;
+			}
+	case 'C':
+			switch(my){
+				case 'X':
+					return 7;
+				case 'Y':
+					return 2;
+				case 'Z':
+					return 6;
+			}
+	}
+	return 0;
+}
